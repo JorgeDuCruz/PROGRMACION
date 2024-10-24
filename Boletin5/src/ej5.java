8b8b306dbbf98cf498208698e4e2ee54911ce2fa
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2024-10-24 14:28:59 +0200

    ejercicio 5 del boletin 5 hecho

---
+++ Boletin5/src/ej5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ej5 {
+    public static void main(String[] args) {
+        int soldo,cantidadeGrupo =0,poucoSoldo =0,iteraciones=-1;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("¿Cual es tu sueldo? (si ya no hay más trabajadores escribe 0)");
+            soldo = sc.nextInt();
+            if (1000<soldo && soldo<=1750) cantidadeGrupo++;
+            if (soldo<1000) poucoSoldo++;
+            iteraciones++;
+        }while (soldo!=0);
+        System.out.println("Hay "+cantidadeGrupo+" personas con un sueldo entre 1000 y 1750€");
+        if (iteraciones!=0)System.out.println("El porcentaje de personas con un sueldo menor a 1000€ es "+poucoSoldo*100/iteraciones+"%");
+    }
+}
