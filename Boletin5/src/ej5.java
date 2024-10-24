3a76ef059771a043ffa3002c404817c017693b44
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2024-10-24 14:31:27 +0200

    cerrado scaner del ej5

--- Boletin5/src/ej5.java
+++ Boletin5/src/ej5.java
@@ -11,6 +11,7 @@ public class ej5 {
             if (soldo<1000) poucoSoldo++;
             iteraciones++;
         }while (soldo!=0);
+        sc.close();
         System.out.println("Hay "+cantidadeGrupo+" personas con un sueldo entre 1000 y 1750€");
         if (iteraciones!=0)System.out.println("El porcentaje de personas con un sueldo menor a 1000€ es "+poucoSoldo*100/iteraciones+"%");
     }
