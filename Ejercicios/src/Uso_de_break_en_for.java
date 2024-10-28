f634707a55d739f02553f7261b80306fd1e3649a
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2024-10-28 13:54:31 +0100

    Ejemplo de uso de break en un bucle while

--- Ejercicios/src/Uso_de_break_en_for.java
+++ Ejercicios/src/Uso_de_break_en_for.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 /**
  * Este código te pide 10 números y te devuelve la suma, pero si quieres parar de introducir números debes escribir 999
- * @version 1.1
+ * @version 2.0
  * @author Jorge Durán Cruz
  */
 public class Uso_de_break_en_for {
@@ -15,6 +15,17 @@ public class Uso_de_break_en_for {
             if (numeroSum==999) break;
             sum=sum+numeroSum;
         }
+
+        System.out.println("La suma de tus numeros es "+sum);
+        sum=0;
+        int i=1;
+        do {
+            System.out.println("Dame el "+i+"º numero  que quieres sumar (Para parar escribe 999)");
+            numeroSum= sc.nextInt();
+            if (numeroSum==999) break;
+            sum=sum+numeroSum;
+            i++;
+        }while (i<=10);
         sc.close();
         System.out.println("La suma de tus numeros es "+sum);
     }
