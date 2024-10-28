bafcdd421f60c539ee38d9c27517a00398aa53c1
Jorge Duran Cruz <jdurancruz@danielcastelao.org>
2024-10-28 18:07:19 +0100

    Ejercicio 7 del boletin 5 hecho

--- Boletin5/src/ej7.java
+++ Boletin5/src/ej7.java
@@ -1,12 +1,18 @@
 /**
  * Este código te pide un número n y te devuelve un triangulo de altura n hecho de *
- * @version 0.1
+ * @version 1.0
  * @author Jorge Durán Cruz
  */
 
+import java.util.Scanner;
+
 public class ej7 {
     public static void main(String[]args){
-        for (int i=1;i<5;i++){
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¿De cúanta altura quieres el triángulo?");
+        n = sc.nextInt();
+        for (int i=1;i<=n;i++){
             for (int j=0;j<i;j++){
                 System.out.print("*");
 
