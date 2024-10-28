2e268f2f5606a27e23fbb807515eeaa79af2434b
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2024-10-28 14:12:36 +0100

    ejercicio 7 del boletin 5 empezado

---
+++ Boletin5/src/ej7.java
@@ -0,0 +1,17 @@
+/**
+ * Este código te pide un número n y te devuelve un triangulo de altura n hecho de *
+ * @version 0.1
+ * @author Jorge Durán Cruz
+ */
+
+public class ej7 {
+    public static void main(String[]args){
+        for (int i=1;i<5;i++){
+            for (int j=0;j<i;j++){
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+}
