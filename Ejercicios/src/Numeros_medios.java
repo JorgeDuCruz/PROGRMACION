b629f0d454485ac0beafe010b9a61d1270ea5340
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2024-10-30 10:37:02 +0100

    Ejercicio para probar el llamar a otra funcion version1.1

--- Ejercicios/src/Numeros_medios.java
+++ Ejercicios/src/Numeros_medios.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 /**
  * Este código te pide dos numeros y te saca por pantalla los numeros entre medias
- * @version 1.0
+ * @version 1.1
  * @author Jorge Durán Cruz
  */
 public class Numeros_medios {
@@ -10,11 +10,20 @@ public class Numeros_medios {
         intermedio();
     }
     static void intermedio(){
+        int min,max,comprobacion;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Dime o número mais baixo");
-        int min = sc.nextInt();
-        System.out.println("Dime o número mais alto");
-        int max = sc.nextInt();
+        System.out.println("Dime un número");
+        min = sc.nextInt();
+        System.out.println("Dime outro número");
+        comprobacion = sc.nextInt();
+        if(min<comprobacion) { //aqui me aseguro de que la variable min sea realmente el numero mas pequeño
+            max=comprobacion;
+        }
+        else { // en caso de min sea mas grande que el otro numero cambio los valores para que sean correctos
+            max = min;
+            min=comprobacion;
+        }
+        System.out.println("Los números comprendidos entre "+min+" y "+max+" son");
         for(min = min+1;min<max;min++){ // aqui se le suma un 1 al numero más bajo para que empiece a contar desde el siguiente y no lo meta en la cuenta
             System.out.println(min);
         }
