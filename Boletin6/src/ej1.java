c71c908f2f09227bf763bff5775818a8d6bad19a
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2024-10-30 10:41:19 +0100

    Ejercicio 1 del boletin 6 hecho

---
+++ Boletin6/src/ej1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ej1 {
+    public static void main(String[]args){
+     intermedio();
+    }
+    static void intermedio(){
+        int min,max,comprobacion;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Dime un número");
+        min = sc.nextInt();
+        System.out.println("Dime outro número");
+        comprobacion = sc.nextInt();
+        if(min<comprobacion) { //aqui me aseguro de que la variable min sea realmente el numero mas pequeño
+            max=comprobacion;
+        }
+        else { // en caso de min sea mas grande que el otro numero cambio los valores para que sean correctos
+            max = min;
+            min=comprobacion;
+        }
+        System.out.println("Los números comprendidos entre "+min+" y "+max+" son");
+        for(min = min+1;min<max;min++){ // aqui se le suma un 1 al numero más bajo para que empiece a contar desde el siguiente y no lo meta en la cuenta
+            System.out.println(min);
+        }
+    }
+}
