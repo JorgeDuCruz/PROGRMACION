03fe9eea64866b23f880332803a4f5fe80771ba5
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2024-10-30 10:59:48 +0100

    Ejercicio 1 del boletin 6 terminado

--- Boletin6/src/ej1.java
+++ Boletin6/src/ej1.java
@@ -2,26 +2,24 @@ import java.util.Scanner;
 
 /**
  * Este código te pide dos numeros y te saca por pantalla los numeros entre medias
- * @version 1.1
+ * @version 2.0
  * @author Jorge Durán Cruz
  */
 public class ej1 {
     public static void main(String[]args){
-     intermedio();
-    }
-    static void intermedio(){
-        int min,max,comprobacion;
+        int nMin, nMax;
         Scanner sc = new Scanner(System.in);
         System.out.println("Dime un número");
-        min = sc.nextInt();
+        nMin = sc.nextInt();
         System.out.println("Dime outro número");
-        comprobacion = sc.nextInt();
-        if(min<comprobacion) { //aqui me aseguro de que la variable min sea realmente el numero mas pequeño
-            max=comprobacion;
-        }
-        else { // en caso de min sea mas grande que el otro numero cambio los valores para que sean correctos
-            max = min;
-            min=comprobacion;
+        nMax = sc.nextInt();
+        intermedio(nMin, nMax);
+    }
+    static void intermedio(int min, int max){
+        if(min>max) { //aqui me aseguro de que la variable min sea realmente el numero mas pequeño
+            int aux = max;
+            max =min;
+            min=aux;
         }
         System.out.println("Los números comprendidos entre "+min+" y "+max+" son");
         for(min = min+1;min<max;min++){ // aqui se le suma un 1 al numero más bajo para que empiece a contar desde el siguiente y no lo meta en la cuenta
