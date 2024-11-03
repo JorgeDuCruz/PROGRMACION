dc124baed691cc2d4c6babe23d6643569d0187d5
Jorge Duran Cruz <jdurancruz@danielcastelao.org>
2024-11-03 19:32:26 +0100

    Ejercicio 2 del boletin 2 hecho

---
+++ Primeiros_Boletins/src/Boletin2_2.java
@@ -0,0 +1,11 @@
+/**
+ * Este código calcula el área de un cuadrado de lado 3
+ * @version 1.0
+ * @author Jorge Durán Cruz
+ */
+public class Boletin2_2 {
+    public static void main(String[]args){
+        int lado=3;
+        System.out.println("El área del cuadrado es de "+lado*lado+" metros cuadrados");
+    }
+}
