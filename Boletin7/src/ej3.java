5f162a3ce6b2b02811b65d89546b941aba77f4ff
Jorge Duran Cruz <jdurancruz@danielcastelao.org>
2024-11-16 17:18:31 +0100

    ejercicio 3 boletin 7 hecho

--- Boletin7/src/ej3.java
+++ Boletin7/src/ej3.java
@@ -10,7 +10,7 @@ public class ej3 {
                 "Sofía", "Javier", "Clara", "Antonio", "Isabel",
                 "Francisco", "Raquel", "Roberto", "Carmen", "Ricardo",
                 "Verónica", "Felipe", "Marta", "Alberto", "Teresa",
-                "Tomás", "Nuria", "Gerardo", "Paula", "Luis", "Beatriz"
+                "Tomás", "Nuria", "Gerardo", "Paula", "Luis"
         };
         for (int i=0;i<notas.length;i++){
             notas[i] = (int) Math.floor(Math.random()*11);
@@ -27,10 +27,21 @@ public class ej3 {
         }
         System.out.println("La nota de "+nome+" es "+notas[identificador]);
 
-        System.out.println("Aprobados: ");
+        System.out.println("\nAprobados: ");
         for (int i=0;i<notas.length;i++) {
             if (notas[i] >= 5) System.out.println(nombres[i]);
         }
-
+        System.out.println("\nLista ordenada:");
+        listadoOrden(nombres,notas,10);
+    }
+    static void listadoOrden(String[] nombres,int[] notas, int nota){
+        if(nota!=0){
+            for (int i=0;i<nombres.length;i++){
+                if(notas[i]==nota){
+                    System.out.println(nombres[i]+": "+notas[i]);
+                }
+            }
+            listadoOrden(nombres,notas,nota-1);
+        }
     }
 }
