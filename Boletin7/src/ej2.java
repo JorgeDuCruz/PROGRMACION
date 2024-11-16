f24e4c50c51a4f127ffde58ffa2e4420c51f3824
Jorge Duran Cruz <jdurancruz@danielcastelao.org>
2024-11-16 16:41:42 +0100

    ejercicio 2 boletin 7 segunda parte hecha

--- Boletin7/src/ej2.java
+++ Boletin7/src/ej2.java
@@ -1,17 +1,25 @@
+/**
+ * Esta funcion genera 30 notas aleatorias, calcula cunatos suspensos hay, cuantos aprobados hay y cual es
+ * la nota media de la clase
+ * @author Jorge Duran Cruz
+ * @version 1.0
+ */
 public class ej2 {
     public static void main(String[]args){
-        int[] numeros= new int[30];
-        int suspensos =0;
-        int aprobados =0;
-        for (int i=0;i<numeros.length;i++){
-            numeros[i] = (int) Math.floor(Math.random()*11);
+        int[] notas= new int[30];
+        int suspensos =0,aprobados =0,notaMedia=0;
+        for (int i=0;i<notas.length;i++){
+            notas[i] = (int) Math.floor(Math.random()*11);
         }
 
-        for (int numero : numeros) {
+        for (int nota : notas) {
 
-            if (numero < 5) suspensos++;
+            if (nota < 5) suspensos++;
             else aprobados++;
+            notaMedia=notaMedia+nota;
         }
+        notaMedia=notaMedia/notas.length;
         System.out.println("Aprobaron "+ aprobados+" alumnos y suspendieron "+suspensos);
+        System.out.println("La nota media de la clase es "+notaMedia);
     }
 }
