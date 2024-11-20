3d55beeb3b5f268b1d6b9fc1fbcb9aab395a53f0
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2024-11-20 10:24:48 +0100

    boletin7 ejercicio1 correccion ligera

--- Boletin7/src/ej1.java
+++ Boletin7/src/ej1.java
@@ -7,7 +7,7 @@ public class ej1 {
     public static void main(String[]args){
         int[] numeros= new int[6];
         for (int i=0;i<numeros.length;i++){
-            numeros[i] = (int) Math.floor(Math.random()*51);
+            numeros[i] = (int) Math.floor(Math.random()*50)+1;
         }
         for (int i= numeros.length-1;i>=0;i--) System.out.println(numeros[i]);
     }
