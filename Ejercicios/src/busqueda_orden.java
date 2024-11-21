42cf783129f870522d4f7884a5cd1878abb587c0
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2024-11-21 14:47:16 +0100

    Solucion a buscar elemento en tabla ordenada,como ordinograma

--- Ejercicios/src/busqueda_orden.java
+++ Ejercicios/src/busqueda_orden.java
@@ -2,25 +2,25 @@ import java.util.Scanner;
 
 public class busqueda_orden {
     public static void main(String[]args) {
-        int[] tablaOrden = {5, 7, 9, 15, 25, 90, 101};
-        int medio,numero, linf = 0,lsup=tablaOrden.length-1, indice=-1;
+        int[] tablaOrden = {1,3,9,16,21,32,41,93,106};
+        int medio,valor, linf = 0,lsup=tablaOrden.length-1, indice=-1;
         Scanner sc = new Scanner(System.in);
-        numero = sc.nextInt();
+        valor = sc.nextInt();
         sc.close();
         while (linf!=lsup){
             medio=(lsup-linf)/2;
-            if (tablaOrden[medio+linf]==numero){
+            if (tablaOrden[medio+linf]==valor){
                 indice=medio+linf;
                 break;
             }
-            if (tablaOrden[medio+linf]>numero){
+            if (tablaOrden[medio+linf]>valor){
                 lsup=medio+linf-1;
             }
             else {
                 linf=medio+linf+1;
             }
         }
-        if (tablaOrden[linf]==numero){
+        if (tablaOrden[linf]==valor){
             indice=linf;
         }
         System.out.println(indice);
