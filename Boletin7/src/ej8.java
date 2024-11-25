7caadc79b931656b050701db3f05dd8a33ebb665
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2024-11-25 13:41:56 +0100

    ejercicio 8 boletin 7 hecho

---
+++ Boletin7/src/ej8.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class ej8 {
+    public static void main(String[]args){
+        int[] numeros=new int[20];
+        for (int i=0;i<numeros.length;i++){
+            numeros[i]=(int)Math.floor(Math.random()*101);
+        }
+        int[] numPares=arrayPares(numeros);
+        System.out.println(Arrays.toString(numPares));
+    }
+
+    static int[] arrayPares(int[] base){
+        int pares=0,posicion=0;
+        for (int numero:base){
+            if (numero%2==0) pares++;
+        }
+        int[] numPares=new int[pares];
+        for (int i=0;i< base.length;i++){
+            if (base[i]%2==0){
+                numPares[posicion]=base[i];
+                posicion++;
+            }
+        }
+        return numPares;
+    }
+}
