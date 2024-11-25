c93ca02ba2e9fc1a39c22804c2ad032fe3d3c2d5
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2024-11-25 14:41:30 +0100

    ejercicio 8 boletin 7 hecho ligeras correcciones

--- Boletin7/src/ej8.java
+++ Boletin7/src/ej8.java
@@ -4,19 +4,19 @@ public class ej8 {
     public static void main(String[]args){
         int[] numeros=new int[(int)(Math.random()*101)];
         for (int i=0;i<numeros.length;i++){
-            numeros[i]=(int)Math.floor(Math.random()*101);
+            numeros[i]=(int)(Math.random()*101);
         }
         int[] numPares=arrayPares(numeros);
         System.out.println(Arrays.toString(numPares));
     }
 
     static int[] arrayPares(int[] base){
-        int pares=0,posicion=0;
+        int pares=0;
         for (int numero:base){
             if (numero%2==0) pares++;
         }
         int[] numPares=new int[pares];
-        for (int i=0;i< base.length;i++){
+        for (int i=0, posicion=0;i< base.length;i++){
             if (base[i]%2==0){
                 numPares[posicion]=base[i];
                 posicion++;
