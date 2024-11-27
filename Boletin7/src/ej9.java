ce5364e0ebbddbb9cd606a28e4ecd45ef556de18
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2024-11-27 09:45:01 +0100

    ejercicio 9 boletin 7 hecho

--- Boletin7/src/ej9.java
+++ Boletin7/src/ej9.java
@@ -15,44 +15,26 @@ public class ej9 {
         System.out.println(Arrays.toString(tabla));
     }
 
-    static int[] tablaSinRepetir(int[] base){
-        int[] copiaBase=new int[base.length];
-        int lonxitude= base.length;
-        for (int i=0;i< base.length;i++){
-            for (int u=i+1;u<base.length;u++){
-                if (base[i]==base[u] && copiaBase[u]!=-1){
-                    copiaBase[u]=-1;
+    static int[] tablaSinRepetir(int[] base) {
+        int[] copiaBase = new int[base.length];
+        int lonxitude = base.length;
+        for (int i = 0; i < base.length; i++) {
+            for (int u = i + 1; u < base.length; u++) {
+                if (base[i] == base[u] && copiaBase[u] != -1) {
+                    copiaBase[u] = -1;
                     lonxitude--;
                     break;
                 }
             }
         }
-        int[] tabla=new int[lonxitude];
-        for (int i=0,posicion=0;i<base.length;i++){
-            if (copiaBase[i]==0){
-                tabla[posicion]=base[i];
+        int[] tabla = new int[lonxitude];
+        for (int i = 0, posicion = 0; i < base.length; i++) {
+            if (copiaBase[i] == 0) {
+                tabla[posicion] = base[i];
                 posicion++;
             }
         }
 
         return tabla;
     }
-    /*
-
-    static int[] tablaNoRepetir(int[] base){
-        int[] copia=new int[base.length];
-        int[] auxiliar=new int[base.length];
-        for (int i=0;i< base.length;i++){
-            for (int u=0;u<base.length;u++){
-                if (base[i]==base[u] && i!=u){
-                    copia=new int[copia.length-1];
-                    System.arraycopy(base,0,copia,0,u-1);
-                    System.arraycopy(base,u+1,copia,u,copia.length-u);
-                    System.arraycopy(copia,0,auxiliar,0,copia.length);
-                }
-            }
-        }
-        return base;
-    }
-    */
 }
