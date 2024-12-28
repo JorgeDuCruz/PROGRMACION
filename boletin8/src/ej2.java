2714dde403a34ade05055df5c42133b9beb01b72
Jorge Duran Cruz <jdurancruz@danielcastelao.org>
2024-12-28 15:54:19 +0100

    boletin 8 ejercicio 2

---
+++ boletin8/src/ej2.java
@@ -0,0 +1,8 @@
+public class ej2 {
+    public static void main(String[] args){
+        String palabra = "Java";
+        for (int i = 0;i<palabra.length();i++){
+            System.out.println(palabra.charAt(i));
+        }
+    }
+}
