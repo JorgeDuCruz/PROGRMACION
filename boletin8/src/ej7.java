6950df8c92d1502166388dd9849be6b05beca4ce
Jorge Duran Cruz <jdurancruz@danielcastelao.org>
2024-12-28 16:31:12 +0100

    boletin 8 ejercicio 7

---
+++ boletin8/src/ej7.java
@@ -0,0 +1,9 @@
+public class ej7 {
+    public static void main(String[]args){
+        String fraseOri = " javeros", fraseMinus = "", fraseMayus = "";
+        fraseMayus = fraseOri.toUpperCase();
+        System.out.println(fraseMayus);
+        fraseMinus = fraseMayus.toLowerCase();
+        System.out.println(fraseMinus);
+    }
+}
