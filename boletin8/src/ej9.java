924612009f7bedad31074e2d6b7f0349bbb0f398
Jorge Duran Cruz <jdurancruz@danielcastelao.org>
2024-12-28 16:38:59 +0100

    boletin 8 ejercicio 9

---
+++ boletin8/src/ej9.java
@@ -0,0 +1,7 @@
+public class ej9 {
+    public static void main(String[]args){
+        String frase = "Jeve jeve jeve";
+        frase = frase.replace('e','a');
+        System.out.println(frase);
+    }
+}
