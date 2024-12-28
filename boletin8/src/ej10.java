84e155b88d6472830934eb0c8fc09f91cdd385f6
Jorge Duran Cruz <jdurancruz@danielcastelao.org>
2024-12-28 16:42:06 +0100

    boletin 8 ejercicio 10

---
+++ boletin8/src/ej10.java
@@ -0,0 +1,8 @@
+public class ej10 {
+    public static void main(String[]args){
+        String letras = "ABCD";
+        for (int i = 0;i<letras.length();i++){
+            System.out.println((int) letras.charAt(i));
+        }
+    }
+}
