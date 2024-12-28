21adda41617410cd762c05ff200f017f85931086
Jorge Duran Cruz <jdurancruz@danielcastelao.org>
2024-12-28 16:55:58 +0100

    boletin 8 ejercicio 11

---
+++ boletin8/src/ej11.java
@@ -0,0 +1,18 @@
+public class ej11 {
+    public static void main(String[]args){
+        String frase = "Ola, son alumno de DAM1, e son programador desde o 2024";
+        int letras = 0, espacios = 0,digitos = 0;
+        for (int i = 0; i<frase.length();i++){
+            char letra = frase.charAt(i);
+            if (letra == ' '){
+                espacios++;
+            } else if (Character.isDigit(letra)) {
+                digitos++;
+            }else if (letra >= 'a' && letra <= 'z' || letra >= 'A' && letra <= 'Z' ){
+                letras++;
+            }
+        }
+        System.out.println(frase.length());
+        System.out.println("Letras: "+letras+ " \nEspacios en blanco: "+espacios+"\nNúmeros: "+digitos);
+    }
+}
