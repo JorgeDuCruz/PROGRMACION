1434795d28b71109fc3fbae33460a0e58ce301ec
Jorge Duran Cruz <jdurancruz@danielcastelao.org>
2024-12-28 15:57:25 +0100

    boletin 8 ejercicio 3

---
+++ boletin8/src/ej3.java
@@ -0,0 +1,8 @@
+public class ej3 {
+    public static void main(String[] args){
+        String frase = "Java dende 0";
+        for (int i=frase.length()-1;i>=0;i--){
+            System.out.println(frase.charAt(i));
+        }
+    }
+}
