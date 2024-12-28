22cbe3bee3b9aa7f88d0264be15e91153f00d00e
Jorge Duran Cruz <jdurancruz@danielcastelao.org>
2024-12-28 16:31:56 +0100

    boletin 8 ejercicio 7 sin redundancias

--- boletin8/src/ej7.java
+++ boletin8/src/ej7.java
@@ -1,6 +1,6 @@
 public class ej7 {
     public static void main(String[]args){
-        String fraseOri = " javeros", fraseMinus = "", fraseMayus = "";
+        String fraseOri = " javeros", fraseMinus, fraseMayus;
         fraseMayus = fraseOri.toUpperCase();
         System.out.println(fraseMayus);
         fraseMinus = fraseMayus.toLowerCase();
