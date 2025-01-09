9412d9e3e1f85fa83288f8cbaf576924991cb39a
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2025-01-09 14:49:43 +0100

    boletin 8v2 ejercicio 3 falta limitar bien QuitarDigito

---
+++ boletin8v2/src/ej3.java
@@ -0,0 +1,63 @@
+public class ej3 {
+    public static void main(String[] args){
+        String cadena = "hola jejej";
+        char separador = ',';
+        int limite =2;
+
+        System.out.println(Separar(cadena,separador,limite));
+
+        separador ='_';
+
+        System.out.println(Remplazar(cadena,separador,limite));
+
+        cadena = "123jo 4.5,4590";
+        separador = 'X';
+
+        System.out.println(QuitarDigito(cadena,separador,limite));
+
+        cadena="25525510026";
+        separador ='.';
+
+        System.out.println(Anhadir(cadena,separador,limite));
+    }
+    static String Separar(String cadena, char separador, int lim){
+        StringBuilder cadea = new StringBuilder(cadena);
+        for (int i=1;i<cadea.length() && lim>0;i+=2,lim--){
+            cadea = new StringBuilder(cadea).insert(i,separador);
+        }
+        System.out.println(cadea);
+        return cadea.toString();
+    }
+    static String Remplazar(String cadena,char separador, int lim){
+        String cad = "";
+        for (int i=0;i<cadena.length() && lim>0;i++){
+            if (cadena.charAt(i)==' '){
+                lim--;
+                cad=cad+separador;
+            }
+            else cad=cad+cadena.charAt(i);
+
+        }
+        return cad;
+    }
+    static String QuitarDigito(String cadena, char separador, int lim){
+        String cad = "";
+        for (int i=0;i<cadena.length() && lim>0;i++){
+            if (Character.isDigit(cadena.charAt(i))){
+                lim--;
+                cad=cad+separador;
+            }
+            else cad=cad+cadena.charAt(i);
+
+        }
+        return cad;
+    }
+    static String Anhadir(String cadena, char separador, int lim){
+        StringBuilder cadea = new StringBuilder(cadena);
+        for (int i=3;i<cadea.length() && lim>0;i+=4,lim--){
+            cadea = new StringBuilder(cadea).insert(i,separador);
+        }
+        System.out.println(cadea);
+        return cadea.toString();
+    }
+}
