05a66a25b9000558eee1203b7664089b00bb63a0
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2025-01-13 14:34:33 +0100

    juego del ahorcado hecho, version simple

---
+++ Ejercicios/src/Ahorcado.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Ahorcado {
+    public static void main(String[] args){
+        String palabra;
+        String letrasDescubiertas ="", adivinacion;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("introduce una plabra");
+            palabra= sc.next();
+
+        }while (palabra.isBlank());
+
+        palabra = palabra.toUpperCase();
+
+        char [] desglose = palabra.toCharArray();
+
+        int intentos = 7;
+
+        do {
+            mostrarPalabra(desglose,letrasDescubiertas.toUpperCase());
+
+            System.out.println("Dime una letra o la palabra");
+
+            adivinacion = sc.next().toUpperCase();
+
+            if (adivinacion.length() == 1) {
+
+                letrasDescubiertas = letrasDescubiertas + adivinacion;
+            }
+            else if (adivinacion.equals(palabra)) {
+                System.out.println("Ganaste la palabra era: " + palabra);
+                break;
+            }
+            intentos--;
+
+            System.out.println("Tienes "+intentos+" intentos restantes");
+
+        }while (intentos>0);
+
+        sc.close();
+
+    }
+
+    private static void mostrarPalabra(char [] palabra, String letras) {
+        for (int i =0;i<palabra.length;i++){
+
+            if (letras.contains(""+palabra[i])){
+
+                System.out.print(palabra[i]);
+            }
+            else System.out.print("_");
+        }
+        System.out.println();
+
+    }
+
+}
