40845bc95caf737c482dc23ada5c2e419c54cdc1
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2025-01-16 14:45:02 +0100

    boletin 8v2 ejercicio 11 Cambio de formato (Mayuscula solo las iniciales)

--- boletin8v2/src/ej11.java
+++ boletin8v2/src/ej11.java
@@ -1,15 +1,17 @@
 public class ej11 {
     public static void main(String []args){
-        String nome = "Jo  rge";
-        String apelido = "Duran";
+        String nome = "jo rGe";
+        String apelido = "duran";
         System.out.println(FormatearNome(nome,apelido));
     }
 
     private static String FormatearNome(String nome, String apelido) {
         String formateo = null;
         nome = nome.replace(" ","");
+        nome = Character.toUpperCase(nome.charAt(0))+nome.toLowerCase().substring(1);
         apelido = apelido.replace(" ","");
-        formateo = nome.toUpperCase()+" "+apelido.toUpperCase();
+        apelido = Character.toUpperCase(apelido.charAt(0))+apelido.toLowerCase().substring(1);
+        formateo = nome+" "+apelido;
         return formateo;
     }
 }
