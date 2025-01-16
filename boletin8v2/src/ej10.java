f7246a2c3595feefd7ad4c69cb337a17816aa717
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2025-01-16 13:35:54 +0100

    boletin 8v2 ejercicio 10

---
+++ boletin8v2/src/ej10.java
@@ -0,0 +1,22 @@
+public class ej10 {
+    public static void main(String[] args){
+        String  contrasinal = "jjjj";
+        System.out.println(ValidarContrsinal(contrasinal));
+    }
+
+    private static boolean ValidarContrsinal(String contra) {
+        boolean valido = false;
+        if (contra.length()>7){
+            if (!contra.toLowerCase().equals(contra) && !contra.toUpperCase().equals(contra)){
+                for (int i=0;i<11;i++){
+                    if (contra.contains(""+i)){
+                        valido = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return valido;
+    }
+}
