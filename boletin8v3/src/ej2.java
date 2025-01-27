e1ee70429e1e2a267ada2bc02b1c8e690b7d28e4
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2025-01-27 13:24:47 +0100

    Ej 2 boletin 8.3 hecho pero no se si esta bien

---
+++ boletin8v3/src/ej2.java
@@ -0,0 +1,18 @@
+import javax.swing.text.html.HTML;
+
+public class ej2 {
+    public static void main(String[] args){
+        System.out.println(CrearHTML("""
+                Esto es el titulo
+                Esto es un parrafo
+                y esto sigue siendo parrafo"""));
+    }
+
+    private static String CrearHTML(String texto) {
+        String html;
+        int titulo = texto.indexOf("\n");
+        html = "<h1>"+texto.substring(0,titulo).trim()+"</h1>"+"\n"+"<p>"+texto.substring(titulo+1).trim()+"</p>";
+
+        return html;
+    }
+}
