dfa9010c3f2eb147f8b7e79c8a0f8701b26198de
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2025-01-27 13:06:33 +0100

    Ej 1 boletin 8.3 corregido return

--- boletin8v3/src/ej1.java
+++ boletin8v3/src/ej1.java
@@ -1,14 +1,16 @@
+import java.util.Arrays;
+
 public class ej1 {
     public static void main(String[] args){
-        System.out.println(DIvidirPalabra("frigorifico",5));
+        System.out.println(Arrays.toString(DIvidirPalabra("frigorifico", 5)));
     }
 
-    private static String DIvidirPalabra(String palabra, int i) {
-        String palabras=palabra.substring(0,i);
-        for (int j=1;j<palabra.length()-(i-1);j++){
-            palabras = palabras+","+palabra.substring(j,j+i);
+    private static String[] DIvidirPalabra(String palabra, int i) {
+        String [] palabras = new String[palabra.length()-(i-1)];
+        for (int j=0;j<palabra.length()-(i-1);j++){
+            palabras[j] = palabra.substring(j,j+i);
         }
 
-        return palabras+".";
+        return palabras;
     }
 }
