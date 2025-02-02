4c072a91a7a4b36d7a0df117374cc12c440bf426
Jorge Duran Cruz <jdurancruz@danielcastelao.org>
2025-02-02 17:51:05 +0100

    Ejercicio 6 del Boletin 8v2 hecho apartado 2

--- boletin8v2/src/ej6.java
+++ boletin8v2/src/ej6.java
@@ -1,6 +1,24 @@
 public class ej6 {
     public static void main(String[] args) {
         System.out.println(Consonates("Logaritmos"));
+        System.out.println(Vocales("Logaritmos"));
+        System.out.println(VocalSiguiente("vestiario"));
+    }
+
+    private static String VocalSiguiente(String cadena) {
+        String cambio="";
+
+        return cambio;
+    }
+
+    private static String Vocales(String cadena) {
+        String vocales = "";
+        for (int i =0;i<cadena.length();i++){
+            if (Character.toLowerCase(cadena.charAt(i))=='a' || Character.toLowerCase(cadena.charAt(i))=='e' ||Character.toLowerCase(cadena.charAt(i))=='i' ||Character.toLowerCase(cadena.charAt(i))=='o' || Character.toLowerCase(cadena.charAt(i))=='u'){
+                vocales = vocales+cadena.charAt(i);
+            }
+        }
+        return vocales;
     }
 
     private static String Consonates(String cadena) {
