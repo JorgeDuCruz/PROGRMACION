6ab3b11817e04ba067ddbbf837a7a40bba24955d
Jorge Duran Cruz <jdurancruz@danielcastelao.org>
2025-02-02 18:15:48 +0100

    Ejercicio 7 del Boletin 8v2 hecho apartado 2

--- boletin8v2/src/ej7.java
+++ boletin8v2/src/ej7.java
@@ -1,6 +1,18 @@
 public class ej7 {
     public static void main(String[] args) {
         System.out.println(IsSubCadea("subcadea","cadea"));
+        System.out.println(Primera("kde","Gnome"));
+    }
+
+    private static String Primera(String cad1, String cad2) {
+        if (cad1.compareToIgnoreCase(cad2)<0){
+            return cad1;
+        } else if (cad2.compareToIgnoreCase(cad1)<0) {
+            return cad2;
+        }
+        else {
+            return "Son la misma palabra";
+        }
     }
 
     private static boolean IsSubCadea(String cadena, String subcadena) {
