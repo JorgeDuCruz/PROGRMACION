6788b178737c766bd8fd15dc79f12df8786e837c
Jorge Duran Cruz <jdurancruz@danielcastelao.org>
2025-02-02 17:38:26 +0100

    Ejercicio 5 del Boletin 8v2 hecho entero

--- boletin8v2/src/ej5.java
+++ boletin8v2/src/ej5.java
@@ -2,6 +2,18 @@ public class ej5 {
     public static void main(String[] args) {
         System.out.println(PrimeraLetra("Universal serial bus"));
         System.out.println(PrimeraMayus("republica argetina"));
+        System.out.println(PalbrasPorA("Antes de abrir"));
+    }
+
+    private static String PalbrasPorA(String cadena) {
+        String palabrasPorA = "";
+        String[] palabras = cadena.split(" ");
+        for (String palabra:palabras){
+            if (palabra.charAt(0)=='a' || palabra.charAt(0)=='A'){
+                palabrasPorA=palabrasPorA+palabra+" ";
+            }
+        }
+        return palabrasPorA;
     }
 
     private static String PrimeraMayus(String cadena) {
