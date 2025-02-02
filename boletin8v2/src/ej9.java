0bd15741c8fa24482cad51b54b629826eda950bf
Jorge Duran Cruz <jdurancruz@danielcastelao.org>
2025-02-02 18:57:08 +0100

    Ejercicio 9 del Boletin 8v2 hecho apartado 2

--- boletin8v2/src/ej9.java
+++ boletin8v2/src/ej9.java
@@ -1,6 +1,12 @@
 public class ej9 {
     public static void main(String[] args) {
         System.out.println(Reemplazar("Cadena de Ejemplo",'e'));
+        System.out.println(Remaracar("Cadena de Ejemplo",'e'));
+    }
+
+    private static String Remaracar(String cadena, char remplazo) {
+        String cad = cadena.replace(String.valueOf(remplazo),"-"+remplazo+"-");
+        return cad;
     }
 
     private static String Reemplazar(String cadena, char remplazo) {
