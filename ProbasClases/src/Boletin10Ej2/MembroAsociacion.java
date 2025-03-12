d74693755c1adcaf8ff97f77bf81af5a2d0a452c
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2025-03-12 09:10:58 +0100

    añadida comprobacion del DNI

--- ProbasClases/src/Boletin10Ej2/MembroAsociacion.java
+++ ProbasClases/src/Boletin10Ej2/MembroAsociacion.java
@@ -5,7 +5,16 @@ public abstract class MembroAsociacion {
     private String nome;
 
     public MembroAsociacion(String dni, String nome) {
-        this.dni = dni;
+        setDni(dni);
+        setNome(nome);
+    }
+
+    public void setDni(String dni) {
+        if (dni.length()==9 && Character.toUpperCase(dni.charAt(8))==calcularLetra(dni)) this.dni = dni;
+        else this.dni="00000000A";
+    }
+
+    public void setNome(String nome) {
         this.nome = nome;
     }
 
@@ -15,6 +24,13 @@ public abstract class MembroAsociacion {
 
     public String getNome() {
         return nome;
+    }
+
+    private char calcularLetra(String nif) {
+        String letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+        //La letra correspondiente será el resto de la división del número del DNI entre las 23 posibilidades que hay
+        char letra = letras.charAt(Integer.parseInt(nif.substring(0,8)) % letras.length());
+        return letra;
     }
 
     public abstract String aCadea();
