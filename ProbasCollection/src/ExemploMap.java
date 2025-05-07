93f38d3e14a051e4bf7addd1a0ac67b251c1e817
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2025-05-07 10:28:18 +0200

    Ejemplos de diversos metodos y acciones de la clase Map

---
+++ ProbasCollection/src/ExemploMap.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class ExemploMap {
+    public static void main(String[] args) {
+        Map<String,Integer> mapa = new TreeMap<>();
+        mapa.put("Ana",23);
+        mapa.put("Jose",32);
+        mapa.put("Ramón",13);
+        mapa.put("Jose",25);
+        System.out.println(mapa);
+        System.out.println(mapa.get("Ana"));
+        mapa.remove("Jose");
+        System.out.println(mapa);
+        System.out.println(mapa.containsKey("Ana"));
+        System.out.println(mapa.containsValue(23));
+
+        //Vistas
+        Set<String> claves = mapa.keySet();
+        System.out.println(claves);
+        Collection<Integer> valores = mapa.values();
+        System.out.println(valores);
+        Set<Map.Entry<String,Integer>> entradas = mapa.entrySet();
+        System.out.println(entradas);
+        for (Map.Entry<String,Integer> entrada:entradas){
+            System.out.println(entrada);
+        }
+
+        Iterator<Map.Entry<String,Integer>> it;
+        for (it = entradas.iterator();it.hasNext();){
+            Map.Entry<String,Integer> elemento = it.next();
+            System.out.println("Llave "+elemento.getKey());
+            System.out.println("Valor "+elemento.getValue());
+        }
+
+        for (String clave:claves){
+            System.out.println("LLave: "+clave+" Valor: "+mapa.get(clave));
+        }
+    }
+}
